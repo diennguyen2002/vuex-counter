33adea3e1e71a9312db5689e8a9969ea45eb84ec
diennvt <diennvt@hybrid-technologies.vn>
2020-09-28 11:24:04 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div> Current count: {{ $store.state.count }} is {{ evenOrOdd }} number</div>
+    <controls />
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+import Controls from './components/Controls'
+export default {
+  name: 'app',
+  components:{
+    Controls
+  },
+  computed: mapGetters([
+    'evenOrOdd'
+  ])
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Controls.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="controls">
+        <input type="button" value="Increment" @click="increment">
+        <input type="button" value="Decrement" @click="decrement">
+    </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+    name: 'controls',
+    methods: mapActions([
+        'increment',
+        'decrement'
+    ])
+
+}
+</script>
+
+<style>
+
+</style>
